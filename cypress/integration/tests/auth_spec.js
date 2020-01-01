f22f289249214b5a999ebe1ac6692b4dfd3581de
Chris Shyi <chris.shyi@mail.utoronto.ca>
2020-01-01 14:15:28 +0800

    Add test for org creation
    Added Cypress test for creating an organization with an admin.

--- cypress/integration/tests/auth_spec.js
+++ cypress/integration/tests/auth_spec.js
@@ -1,7 +1,16 @@
 /// <reference types="Cypress" />
+before(() => {
+    cy.exec("node dropDB.js");
+    cy.exec("node scripts/seedLeaves.js");
+});
+
+after(() => {
+    cy.exec("node dropDB.js");
+    cy.exec("node scripts/seedLeaves.js");
+})
 
 import { testUserLogin } from './utils';
-describe("Tests authentication functionality", function() {
+describe("Test authentication functionality", function() {
 
     it("User can log in properly", () => {
         cy.visit('/');
@@ -15,6 +24,7 @@ describe("Tests authentication functionality", function() {
         cy.contains("Logout"); // checks that the user is logged in properly
     });
     it("Incorrect credentials prevent the user from logging in", () => {
+        
         cy.visit('/');
 
         cy.contains('Login').click();
@@ -32,4 +42,15 @@ describe("Tests authentication functionality", function() {
         cy.contains('Login');
         cy.contains('Logout').should('not.visible');
     });
+    it("User can create new organization and admin", () => {
+        cy.visit('/');
+        cy.contains("Sign Up").click();
+        cy.contains("Name of Organization").next().type("New Org");
+        cy.contains("Email").next().type("newOrgAdmin@gmail.com");
+        cy.contains("Password").next().type("123456");
+        cy.contains(/^Name$/).next().type("NewOrgAdmin");
+        cy.contains("Title").next().type("Blah");
+        cy.contains("Submit").click();
+        cy.contains("Welcome NewOrgAdmin");
+    })
 });
