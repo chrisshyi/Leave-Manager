90c8c92d8e73164dfc9b6c2ad04c6c750d762994
Chris Shyi <chris.shyi@mail.utoronto.ca>
2020-01-08 12:48:04 +0800

    Changed mongo connection string

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const config = require('config');
 const PORT = process.env.PORT || 5000;
 const testing = config.get("testing"); 
 const local = config.get('local');
-const path = require('path');
+// const path = require('path');
 
 if (testing) {
     connectTestDB(local);
@@ -21,12 +21,12 @@ app.use('/api/leaves', require('./routes/api/leaves'));
 app.use('/api/orgs', require('./routes/api/orgs'));
 
 // Serve React file if in production mode
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+//if (process.env.NODE_ENV === 'production') {
+//    app.use(express.static('client/build'));
+//    app.get('*', (req, res) => {
+//        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//    });
+//}
 
 if (!testing) {
     const server = app.listen(PORT, function() {
